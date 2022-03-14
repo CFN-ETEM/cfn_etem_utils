10b022fc190145deca4fa8d6fc728d4493f983c4
Xiaohui Qu <xq@earth.org>
2022-03-14 18:08:37 -0400

    add entry point

--- cfntem/cmd/read_K2.py
+++ cfntem/cmd/read_K2.py
@@ -6,3 +6,6 @@ def main():
     filepath_input = "/home/xiaqu/Documents/data/Converter_Project/Capture6/Capture6_.gtg"
     datacube = py4DSTEM.io.read(filepath_input, mem='MEMMAP')
     print("not implemented")
+
+if __name__ == '__main__':
+    main()
