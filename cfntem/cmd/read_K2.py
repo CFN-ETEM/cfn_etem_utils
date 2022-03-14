ca5e2832788ce2726a4756fa1742897052f61f26
Xiaohui Qu <xq@earth.org>
2022-03-14 18:03:45 -0400

    read example file

--- cfntem/cmd/read_K2.py
+++ cfntem/cmd/read_K2.py
@@ -1,2 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import py4DSTEM
+
 def main():
+    filepath_input = "/home/xiaqu/Documents/data/Converter_Project/Capture6/Capture6_.gtg"
+    datacube = py4DSTEM.io.read(filepath_input, mem='MEMMAP')
     print("not implemented")
