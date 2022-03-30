72e8ff2a60c3643dbaa1b3e0d3d0e19773c1a659
Xiaohui Qu <xq@earth.org>
2022-03-30 09:42:43 -0400

    the name begins with K2

--- cfntem/cmd/read_K2.py
+++ cfntem/cmd/read_K2.py
@@ -3,7 +3,7 @@ from cfntem.io.read_K2 import read_gatan_K2_bin
 
 def main():
     filepath_input = "/home/xiaqu/Documents/data/Converter_Project/Capture6/Capture6_.gtg"
-    datacube = read_gatan_K2_bin(filepath_input, mem='MEMMAP', sync_block_IDs=False)
+    datacube = read_gatan_K2_bin(filepath_input, mem='MEMMAP', K2_sync_block_IDs=False)
     print(datacube.data.shape)
 
 if __name__ == '__main__':
