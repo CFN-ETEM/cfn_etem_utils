f032fb687680d3d145ffc5109742150e3cd7ceaf
Xiaohui Qu <xq@earth.org>
2022-03-30 13:47:08 -0400

    disable background substraction by default

--- cfntem/cmd/read_K2.py
+++ cfntem/cmd/read_K2.py
@@ -3,7 +3,7 @@ from cfntem.io.read_K2 import read_gatan_K2_bin
 
 def main():
     filepath_input = "/home/xiaqu/Documents/data/Converter_Project/Capture6/Capture6_.gtg"
-    datacube = read_gatan_K2_bin(filepath_input, mem='MEMMAP', K2_sync_block_IDs=False)
+    datacube = read_gatan_K2_bin(filepath_input, mem='MEMMAP', K2_sync_block_IDs=False, K2_hidden_stripe_noise_reduction=False)
     print(datacube.data.shape)
 
 if __name__ == '__main__':
