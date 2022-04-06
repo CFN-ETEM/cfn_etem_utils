b704f1284d200e3158b3aafe6a5555f18b0f1ebf
Xiaohui Qu <xq@earth.org>
2022-04-06 14:51:35 -0400

    ad requirements

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup
 
+with open("requirements.txt") as f_req:
+    required_list = [line.rstrip() for line in f_req.readlines()]
+    
 setup(
     name='CFN ETEM',
     version='0.1',
@@ -10,6 +13,7 @@ setup(
     author_email='xiaqu@bnl.gov',
     description='CFN ETEM Analytics',
     python_requires='>=3.8',
+    install_requires=required_list,
     entry_points={
         "console_scripts": [
             "read_K2 = cfntem.cmd.read_K2:main"
