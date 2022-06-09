b99ed3d58817ef5164f7ff03083c2d8e436fd191
Xiaohui Qu <xq@earth.org>
2022-06-09 10:25:31 -0400

    fix ipp parameters

--- cfntem/cmd/wait_ipp_engines.py
+++ cfntem/cmd/wait_ipp_engines.py
@@ -5,12 +5,11 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-e', '--engines', type=int, required=True,
                         help='Number of engines')
-    parser.add_argument('-w', "--work_dir", type=str, default='.',
-                        help="Working directory to write the output files")
+    parser.add_argument('--ipp_dir', type=str, default='ipypar',
+                        help='The directory for IpyParallel environment')
     args = parser.parse_args()
-    work_dir = os.path.abspath(os.path.expanduser(args.work_dir))
 
-    c = ipp.Client(connection_info=f"{work_dir}/ipypar/security/ipcontroller-client.json")
+    c = ipp.Client(connection_info=f"{args.ipp_dir}/security/ipcontroller-client.json")
 
     c.wait_for_engines(n=args.engines, timeout=3600)
     
