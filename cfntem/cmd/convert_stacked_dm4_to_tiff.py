5fb9dc2c543bd37499d4ecfd112003c062c5cafa
Xiaohui Qu <xq@earth.org>
2024-04-04 13:43:37 -0400

    add script to convert dm4 to tiff files

---
+++ cfntem/cmd/convert_stacked_dm4_to_tiff.py
@@ -0,0 +1,21 @@
+    
+import argparse, pathlib, os
+import hyperspy.api as hs
+from PIL import Image
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-s", "--source", help="The stacked DM4 file name",
+                        required=True)
+    parser.add_argument("-t", "--to", help="The folder to hold the TIFF files",
+                        required=True)
+    args = parser.parse_args()
+    fn_dm4 = pathlib.Path(args.source)
+    images = hs.load(fn_dm4).data
+    dir_tiff = pathlib.Path(args.to)
+    if not dir_tiff.exists():
+        os.makedirs(dir_tiff, exist_ok=True)
+    for j, img in enumerate(images):
+        Image.fromarray(img).save(dir_tiff / "frame_{j:03d}.tiff")
+
